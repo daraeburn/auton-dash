fcaf921c1afb105634585805ecba38f88246eac3
Graeme Thomson-Gold <graeme.thomson-gold@verint.com>
2017-06-30 14:40:51 +0100

    Adding counts

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Auton Dashboard</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional Bootstrap theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Auton Dashboard</h1>
-        </div>
-    
-        <?php
-        require 'qc.php';
+<?php
+require 'qc.php';
+require 'ui.php';
 
-        $almClient = qcLogin($connectionParams);
-        if ($almClient) {
-            $defects = qcGetDefectsByTag($almClient,'GLA_auto');
-            qcDefectsTable($defects);
-        }
-        else {
-            echo "Not Authenticated - sorry.";
-        }
 
-        qcLogout($almClient);
-        ?>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </div>
-</body>
-</html>
+
+$almClient = qcLogin($connectionParams);
+if ($almClient) {
+    $notClosedDefects = qcGetDefectsByTagAndState($almClient,'GLA_auto','<>Closed');
+
+    $todoDefects = qcGetDefectsByTagAndState($almClient,'GLA_auto','New');
+    $todoCount = count($todoDefects);
+
+    $inProgressDefects = qcGetDefectsByTagAndState($almClient,'GLA_auto','Open');
+    $inProgressCount = count($inProgressDefects);
+
+    $waitingOnDevDefects = qcGetDefectsByTagAndState($almClient,"GLA_auto",urlencode("Ready for Test"));
+    $waitingOnDevCount = count($waitingOnDevDefects);
+}
+head();
+drawTitle();
+drawCountBoxes($todoCount, $inProgressCount, $waitingOnDevCount);
+qcDefectsTable($notClosedDefects);
+qcLogout($almClient);
+drawFooter();
+?>
---
+++ ui.php
@@ -0,0 +1,59 @@
+<?php
+function drawCountBoxes($todoCount, $inProgressCount, $waitingOnDevCount) {
+echo '<div class="container">
+    <!--Row with three equal columns-->
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body"><h2>ToDo</h2><h2>'.$todoCount.'</div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body"><h2>In Progress</h2><h2>'.$inProgressCount.'</h2></div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body"><h2>With reporter</h2><h2>'.$waitingOnDevCount.'</h2></div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body"><h2>Done</h2></div>
+            </div>
+        </div>
+
+    </div>
+</div>';
+}
+
+function drawTitle() {
+echo '<div class="jumbotron">
+<h1>Auton Dashboard</h1>
+</div>';
+}
+
+function head() {
+    echo '<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Auton Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional Bootstrap theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">';
+}
+
+function drawFooter(){
+    echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </div>
+</body>
+</html>';
+}
+?>
